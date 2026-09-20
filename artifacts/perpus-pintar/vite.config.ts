8766d9e487e44b0b3b9232ac95ce69b72358fa14
Irzanour <186652822+Reyhan-irza@users.noreply.github.com>
2026-09-20 16:51:15 +0700

    Clean repository of local artifacts

--- artifacts/perpus-pintar/vite.config.ts
+++ artifacts/perpus-pintar/vite.config.ts
@@ -40,12 +40,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(
-        __dirname,
-        '..',
-        '..',
-        'attached_assets',
-      ),
     },
     dedupe: ['react', 'react-dom'],
   },
